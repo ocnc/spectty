eec6379d02c0eb6daf4a541d13374979d609de58
Deniss Afonin <dafonin@twilio.com>
2026-02-23 13:43:54 +0200

    tests

--- Packages/SpecttyKeychain/Package.swift
+++ Packages/SpecttyKeychain/Package.swift
@@ -4,11 +4,15 @@ import PackageDescription
 
 let package = Package(
     name: "SpecttyKeychain",
-    platforms: [.iOS(.v18)],
+    platforms: [.iOS(.v18), .macOS(.v14)],
     products: [
         .library(name: "SpecttyKeychain", targets: ["SpecttyKeychain"]),
     ],
     targets: [
         .target(name: "SpecttyKeychain"),
+        .testTarget(
+            name: "SpecttyKeychainTests",
+            dependencies: ["SpecttyKeychain"]
+        ),
     ]
 )
